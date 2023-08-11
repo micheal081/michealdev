1110770d0d60e9834dbe7807f90ce11c4623087d
micheal081 <michig3000@gmail.com>
2023-08-11 15:51:14 +0100

    modified content

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,7 +7,7 @@ import './intro.css';
 const Intro = () => {
   return (
     <section id="about">
-      <h5>Get to know</h5>
+      <h5>Get to know me</h5>
       <h2>About Me</h2>
       <div className="container about__container">
         <div className="about__me">
