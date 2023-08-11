f66b39d254385e8779e7ccc1287bb5a3ee66a5a4
micheal081 <michig3000@gmail.com>
2023-08-11 15:38:04 +0100

    modified content

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -25,10 +25,10 @@ const Intro = () => {
             <article className="about__card">
               <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
-              <small>12+ Completed Projects</small>
+              <small>54+ Completed Projects</small>
             </article>
           </div>
-          <p>I am a dedicated and adept software engineer, boasting 5+ years of experience in crafting inventive solutions to drive both business and personal empowerment. My expertise spans diverse areas such as computer organization, data structures, and software design. Through my mastery of Node.JS, TypeScript, JavaScript, and React.JS, I have effectively delivered cutting-edge applications that optimize financial services and elevate user experiences.</p>
+          <p>I am a dedicated and adept backend engineer, boasting 5+ years of experience in crafting inventive solutions to drive both business and personal empowerment. My expertise spans diverse areas such as computer organization, data structures, and software design. Through my mastery of Node.JS, TypeScript, JavaScript, and React.JS, I have effectively delivered cutting-edge applications that optimize financial services and elevate user experiences.</p>
           <a href="#contact" className="btn btn-primary">Let's Talk</a>
         </div>
       </div>
